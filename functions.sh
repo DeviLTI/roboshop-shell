39ad7843830b9a73837b6bb04c6e0ccc2d4631b1
Kdevi <kasdevi2309@gmail.com>
2026-06-15 12:34:43 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-Logfolder = "/var/log/roboshop-shell"
+Logfolder="/var/logs/roboshop-shell"
 Script-name= "$(echo ${0} | cut -d '.' -f1)
 logfile = "Logfolder/Script-name".log
 Script_dir = $PWD
@@ -22,9 +22,9 @@ fi
 validate(){
     if [ $1 -eq 0 ]
     then
-      echo -e "$2 installation ${G} success ${N}  &>>logfile"
+      echo -e "$2 installation ${G} success ${N} " &>>logfile
     else
-      echo -e "$2 installation ${R} failed ${N}  &>>logfile"
+      echo -e "$2 installation ${R} failed ${N}  " &>>logfile
       exit 1
     fi
 }
@@ -34,12 +34,12 @@ validate(){
 dnf list installed mysql &>>logfile
 if [ $? -ne 0 ]
 then
-echo "$2 is not installed...going to install  &>>logfile"
+echo "$2 is not installed...going to install  "&>>logfile
     exit 1
 
     dnf install mysql -y  &>>logfile
     validate $? mysql
 else
-    echo -e "$2 was already installed.. ${Y} Nothing to do ${N}  &>>logfile"
+    echo -e "$2 was already installed.. ${Y} Nothing to do ${N} "&>>logfile
 fi
 
