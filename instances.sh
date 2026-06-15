48ac3bae53022a1b8b3ccddc649675bddb975df1
Kdevi <kasdevi2309@gmail.com>
2026-06-15 15:41:12 +0530

    functions

--- instances.sh
+++ instances.sh
@@ -4,11 +4,19 @@ AMI_ID=ami-0220d79f3f480ecf5
 SG_ID=sg-0f5d722990e27d6eb
 Instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 Zone_ID=Z07085248BXF6N8LMPZW
-Domain_name=daws23.space
+DOMAIN_NAME=daws23.space
+set -xe
 
-for instance in ${instances[@]}
+for instance in ${Instances[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sgr-0202e527062c48de9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=INSTANCE_ID=$(aws ec2 run-instances \
+      --image-id $AMI_ID \
+      --instance-type t3.micro \
+      --security-group-ids $SG_ID \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+      --query "Instances[0].InstanceId" \
+      --output text)
+
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
@@ -25,12 +33,12 @@ echo "$instance IP address: $IP"
     {
         "Comment": "Creating or Updating a record set for cognito endpoint",
         "Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
+        "Action"              : "UPSERT",
+        "ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'",
+            "Type"             : "A",
+            "TTL"              : 1
+            "ResourceRecords"  : [{
                 "Value"         : "'$IP'"
             }]
         }
