8fdfe3c95a229848728a13ea7d70b53cd2ca30a9
Kdevi <kasdevi2309@gmail.com>
2026-06-15 14:59:48 +0530

    functions

--- instances.sh
+++ instances.sh
@@ -7,22 +7,32 @@ Domain_name=daws23.space
 
 for instance in ${instances[@]}
 do
- InstanceID = $(aws ec2 run-instances 
-    --image-id "$AMI_ID" 
-    --instance-type "t3.micro"
-   --securitygroupids "$SG_ID" --tag-specifications
-   "Resourcetype=instance,Tags=[{key=name, value=$instance}]"
-    --query 'Instances[0].InstanceId' 
-    --output text)
-if [ $instance != frontend]
-then 
-echo "IP=$(aws ec2 describe-instance 
-  --query 'Reservations[0].Instances[0].[$InstanceId, PrivateIpAddress]' 
-  --output text)"
-  else
-  echo "IP=$(aws ec2 describe-instance 
-  --query 'Reservations[0].Instances[0].[$InstanceId, PublicIpAddress]' 
-  --output text)"
-  fi
-  echo "$instance IP Address = $IP"
-  done 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sgr-0202e527062c48de9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+echo "$instance IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
+done
