1d52239a68b57630d07833fef4f1397c3a08d959
Kdevi <kasdevi2309@gmail.com>
2026-06-15 12:50:12 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -23,9 +23,9 @@ fi
 validate(){
     if [ $1 -eq 0 ]
     then
-      echo -e "$2 installation ${G} success ${N}" &>>logfile
+      echo -e "$2 installation ${G} success ${N}" | tee -a logfile
     else
-      echo -e "$2 installation ${R} failed ${N}" &>>logfile
+      echo -e "$2 installation ${R} failed ${N}" | tee -a logfile
       exit 1
     fi
 }
@@ -35,9 +35,9 @@ validate(){
 dnf list installed mysql &>>logfile
 if [ $? -ne 0 ]
 then
-echo "$package is not installed...going to install" &>>logfile
+echo "$package is not installed...going to install" | tee -a logfile
 dnf install mysql -y  &>>logfile
 validate $? mysql
 else
-    echo -e "$package was already installed.. ${Y} Nothing to do.. ${N}" &>>logfile
+    echo -e "$package was already installed.. ${Y} Nothing to do.. ${N}" |tee -a logfile
 fi
